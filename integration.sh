89afa4f040605906433607288549d3761c68bbf3
scriptype <scriptyper@gmail.com>
2017-02-15 21:40:34 +0300

    Fix integration script

--- integration.sh
+++ integration.sh
@@ -3,12 +3,12 @@ cp -R __temp_clone__/run run
 node -e "\
   var fs = require('fs'); \
   var srcPackage = require('./__temp_clone__/package.json'); \
-  var devDependencies = srcPackage.devDependencies; \
-  var scripts = srcPackage.scripts; \
+  var { config, scripts, devDependencies } = srcPackage; \
   var targetPackage = require('./package.json'); \
   var newPackage = Object.assign({}, targetPackage, { \
-    scripts, \
-    devDependencies \
+    config: Object.assign({}, targetPackage.config, config), \
+    scripts: Object.assign({}, targetPackage.scripts, scripts), \
+    devDependencies: Object.assign({}, targetPackage.devDependencies, devDependencies) \
   }); \
   fs.writeFileSync('package.json', JSON.stringify(newPackage, null, 2)); \
 " && \
